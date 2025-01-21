30e1b0f7829adda68521fc68e7ce1a87fe94fddd
andrei.antonov <andrei.antonov@corp.mail.ru>
2025-01-21 23:28:37 +0300

    Подключение роутера и рефактор кода

--- src/containers/Modal/Modal.tsx
+++ src/containers/Modal/Modal.tsx
@@ -1,4 +1,5 @@
 import styles from './Modal.module.scss';
+import React from "react";
 
 
 export type ModalProps = {
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,3 +1,11 @@
+import { lazy } from 'react';
+
+const MainPage = lazy(() => import('@/pages/MainPage/MainPage'));
+const LoginPage = lazy(() => import('@/pages/LoginPage/LoginPage'));
+const RegisterPage = lazy(() => import('@/pages/RegisterPage/RegisterPage'));
+
 export const routes = [
-	{ index: true, element: <p>Hello world!</p> }
+	{ index: true, element: <MainPage /> },
+	{ path: '/login', element: <LoginPage /> },
+	{ path: '/register', element: <RegisterPage /> },
 ];
